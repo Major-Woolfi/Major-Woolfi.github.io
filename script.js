f76f4f738553254b7bdedbe929e3d577efdf4343
Major-Woolfi <katuskovkirill@gmail.com>
2025-09-10 19:50:45 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const fadeEls = document.querySelectorAll('.fade-in');
     const onScroll = () => {
         fadeEls.forEach(el => {
